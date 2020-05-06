a747a0fa0fe477ececbdb8419a45f61b633277a1
Poppy <37153363+ppyrinn@users.noreply.github.com>
2020-05-06 08:41:16 +0700

    Create new project

---
+++ MC2-app/MC2-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC2-app
+//
+//  Created by Poppy on 06/05/20.
+//  Copyright © 2020 Poppy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
