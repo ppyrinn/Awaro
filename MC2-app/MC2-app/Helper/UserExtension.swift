fcdd8f6dfd50b9ffb26e7219ce486d331a5641bf
poppy <poppy.hwang@gmail.com>
2020-05-18 09:27:28 +0700

    create new helper files

---
+++ MC2-app/MC2-app/Helper/UserExtension.swift
@@ -0,0 +1,43 @@
+//
+//  UserExtension.swift
+//  Awaro
+//
+//  Created by Poppy on 18/05/20.
+//  Copyright © 2020 Poppy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+extension User{
+    // fungsi tambah data ke core data
+    static func createUser(_ firstName:String, _ lastName:String, _ email:String){
+        
+        // referensi ke AppDelegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        // managed context
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // refensi entity yang telah dibuat sebelumnya
+        let userEntity = NSEntityDescription.entity(forEntityName: "User", in: managedContext)
+        
+        // entity body
+        let insert = NSManagedObject(entity: userEntity!, insertInto: managedContext)
+//        insert.setValue(id, forKey: "userID")
+        insert.setValue(firstName, forKey: "firstName")
+        insert.setValue(lastName, forKey: "lastName")
+        insert.setValue(email, forKey: "email")
+        
+        do{
+            // save data ke entity user core data
+            try managedContext.save()
+            print("\n\nsave data ke entity user core data berhasil\n\n")
+            //                id += 1
+        }catch let err{
+            print(err)
+        }
+        
+    }
+}
