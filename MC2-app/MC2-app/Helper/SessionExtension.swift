452b6763cf73d95c108c17424617153c514ce423
poppy <poppy.hwang@gmail.com>
2020-05-18 20:21:05 +0700

    update session extension

--- MC2-app/MC2-app/Helper/SessionExtension.swift
+++ MC2-app/MC2-app/Helper/SessionExtension.swift
@@ -7,10 +7,32 @@
 //
 
 import Foundation
+import UIKit
 import CoreData
 
 extension Session{
-//    func createSession(_ sessionID:Int, _sessionName:String, _ createdAt:Date) -> <#return type#> {
-//        <#function body#>
-//    }
+    static func createSession(_ sessionID:Int, _ sessionName:String) {
+        // referensi ke AppDelegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        // managed context
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // refensi entity yang telah dibuat sebelumnya
+        let userEntity = NSEntityDescription.entity(forEntityName: "Session", in: managedContext)
+        
+        // entity body
+        let insert = NSManagedObject(entity: userEntity!, insertInto: managedContext)
+        insert.setValue(sessionID, forKey: "sessionID")
+        insert.setValue(sessionName, forKey: "sessionName")
+        
+        do{
+            // save data ke entity user core data
+            try managedContext.save()
+            print("\n\nsave data ke entity user core data berhasil\n\n")
+            //                id += 1
+        }catch let err{
+            print(err)
+        }
+    }
 }
